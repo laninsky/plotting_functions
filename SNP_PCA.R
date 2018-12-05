5857929a3458c8413212174d8a7f02cdba20b2aa
Alana Alexander <laninsky@gmail.com>
2018-12-05 13:24:26 +1300

    Update SNP_PCA.R

--- SNP_PCA.R
+++ SNP_PCA.R
@@ -5,7 +5,21 @@ structure <- read.structure("lamprey.stru",n.ind=38,n.loc=21469,col.lab=1,col.po
 replace_missing <- tab(structure, freq=TRUE,NA.method="mean")
 pca1 <- dudi.pca(replace_missing,center=TRUE,scale=FALSE)
 #select 2 axes
-s.label(pca1$li)
+
+# Option 1 for plotting
+fviz_eig(pca1)
+
+fviz_pca_ind(pca1,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             )
+
+fviz_pca_biplot(pca1, 
+                col.ind = "#696969"  # Individuals color
+                )
+
+# Option 2 for plotting 
+s.label(pca1$li,addaxes=TRUE)
 eig.perc <- 100*pca1$eig/sum(pca1$eig)
 add.scatter.eig(eig.perc,3,1,2, ratio=.3,posi="bottomright")
 #add.scatter.eig(eig.perc,3,1,2, ratio=.3,posi="topright")
